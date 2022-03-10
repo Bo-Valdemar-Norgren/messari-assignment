62872bf7aae0eef25fafe72763cda4c8ff726ba3
Bo Valdemar Norgren <bo.valdemar.norgren@gmail.com>
2022-03-10 16:12:48 +0100

    work on aggregator

--- aggregator.go
+++ aggregator.go
@@ -7,6 +7,7 @@ import (
 )
 
 
+// Probably requires some tracker variables for efficient calculation of meanprice, VWAP, percentageBuyOrder
 type Order struct {
 	id int
 	market int
@@ -19,57 +20,57 @@ type Order struct {
 func main() {
 	input := bufio.NewScanner(os.Stdin)
 	metrics := make(map[int]map[string]float32)
-	totalOrders := 0
-
-	// TODO: Add "structure" of following lines as the default structure for metrics
-	// metrics[marketID]["totalVolume"]
-	// metrics[marketID]["meanPrice"]
-	// metrics[marketID]["meanVolume"]
-	// metrics[marketID]["VWAP"]
-	// metrics[marketID]["percentageBuyOrder"]
 
 	for input.Scan() {
 		line := input.Text()
 
+		// Ready to receive first order
 		if line == "BEGIN" {
 			continue
+		// Last order was reached
 		} else if line == "END" {
 			break
+		// Receieve order
 		} else {
-			var order Order =
-			parseOrderAndUpdateMetrics(metrics, &order)
+			var order Order
+			err := json.Unmarshal([]byte(line), &order)
+			if err != nil {
+				fmt.Println("ERROR: Order could not be unmarshaled.")
+			} else {
+				//fmt.Println(order)
+				processOrder(metrics, &order)
+			}
 		}
 	}
 	outputMetrics(metrics)
 }
 
 
-func parseOrderAndUpdateMetrics(metrics map[int]map[string]float32, order *Order) {
+func processOrder(metrics map[int]map[string]float32, order *Order) {
 	marketID := order.market
 
-	// If market has been traded on
+	// Market has previously been traded on, update metrics for it.
 	if metrics[marketID] {
+		updateMetrics(metrics, order)
 
-
-	// If market has not yet been traded on
+	// First trade on market, initialize metrics for it.
 	} else {
-
+		initializeMetrics(metrics, order)
 	}
 }
 
 
-func initializeMarketMetrics(metrics map[int]map[string]float32, order *Order) {
-	// Probably requires some tracker variables for meanprice, VWAP, percentageBuyOrder
-
-	marketID := 
-	price :=
-	volume :=
-	isBuy :=
+func initializeMetrics(metrics map[int]map[string]float32, order *Order) {
+	marketID := order.market
+	price := order.price
+	volume := order.volume
+	isBuy := order.is_buy
 
 	metrics[marketID] = make(map[string]float32)
 
 	metrics[marketID]["totalVolume"] = volume
 	metrics[marketID]["meanPrice"] = price
+	metrics[marketID]["meanVolume"] = volume
 	metrics[marketID]["VWAP"] = price
 
 	var percentageBuy float32 = 0
@@ -77,20 +78,23 @@ func initializeMarketMetrics(metrics map[int]map[string]float32, order *Order) {
 		percentageBuy = 1
 	}
 	metrics[marketID]["percentageBuyOrder"] = percentageBuy
-
 }
 
 
-func updateMarketMetrics(metrics map[int]map[string]float32, order *Order) {
-	marketID := 
-	price :=
-	volume :=
-	isBuy :=
+func updateMetrics(metrics map[int]map[string]float32, order *Order) {
+	marketID := order.market
+	price := order.price
+	volume := order.volume
+	isBuy := order.is_buy
 
 	// Update total volume
 	metrics[marketID]["totalVolume"] += volume
 
+	// TODO: Find an efficient way of continously updating these metrics
+
 	// Update mean price
+
+	// Update mean volume
 
 	// Update VWAP
 
